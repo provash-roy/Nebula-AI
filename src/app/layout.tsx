be36a752112876d33038695fa76b16fdc9932ebf
Provash-roy <provashray94@gmail.com>
2026-07-12 23:41:47 +0600

    create agent state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/shared/app-sidebar";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/shared/navbar";
