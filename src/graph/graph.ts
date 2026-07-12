9d19a89077c6db91abec17e546aa82e629e367a9
Provash-roy <provashray94@gmail.com>
2026-07-13 02:00:05 +0600

    add agent state graph with nodes and edges

--- src/graph/graph.ts
+++ src/graph/graph.ts
@@ -1,5 +1,49 @@
+import { chatAgent } from "@/agents/chat/chat-agent";
+import { codingAgent } from "@/agents/coding/coding-agent";
+import { imageAgent } from "@/agents/image/image-agent";
+import { routerAgent } from "@/agents/router/router-agent";
+import { searchAgent } from "@/agents/search/search-agent";
 import { agentState } from "@/agents/shared/state";
 import { StateGraph } from "@langchain/langgraph";
 
 const agent = new StateGraph(agentState)
-  
+  .addNode("router", routerAgent)
+  .addNode("chat", chatAgent)
+  .addNode("search", searchAgent)
+  .addNode("coding", codingAgent)
+  .addNode("image", imageAgent)
+  .addEdge("__start__", "router")
+  .addConditionalEdges(
+    "router",
+    (state) => {
+      if (state.agent === "chat") {
+        return "chat";
+      }
+
+      if (state.agent === "search") {
+        return "search";
+      }
+
+      if (state.agent === "coding") {
+        return "coding";
+      }
+
+      if (state.agent === "image") {
+        return "image";
+      }
+
+      return "chat";
+    },
+    {
+      chat: "chat",
+      coding: "coding",
+      search: "search",
+      image: "image",
+    },
+  )
+  .addEdge("search", "chat")
+  .addEdge("chat", "__end__")
+  .addEdge("coding", "__end__")
+  .addEdge("image", "__end__");
+
+export const graph = agent.compile();
